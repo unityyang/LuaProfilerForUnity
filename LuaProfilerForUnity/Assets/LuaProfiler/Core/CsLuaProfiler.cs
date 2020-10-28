d37b24075006b541bcb8e22acbc6e97f784ee8b6
unityyang <766684931@qq.com>
2020-10-29 00:00:10 +0800

    add a project

---
+++ LuaProfilerForUnity/Assets/LuaProfiler/Core/CsLuaProfiler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using MikuLuaProfiler;
+using SLua;
+using UnityEngine;
+
+[CustomLuaClass]
+public interface ICsLuaProfiler
+{
+    void BeginSample(int sampleId);
+    void EndSample();
+}
+
+[CustomLuaClass]
+public class CsLuaProfiler : ICsLuaProfiler
+{
+    static CsLuaProfiler __inst = new CsLuaProfiler();
+
+    public static ICsLuaProfiler CsInstance() { return (ICsLuaProfiler)__inst; }
+
+    [SLua.MonoPInvokeCallbackAttribute(typeof(SLua.LuaCSFunction))]
+    [StaticExport]
+    public static int LuaInstance(IntPtr l)
+    {
+        LuaObject.pushValue(l, true);
+        LuaObject.pushObject(l, CsInstance());
+        return 2;
+    }
+
+    IntPtr ptr_luaState { get { return LuaProfiler.mainL; } } //Get your self lua state ptr
+
+    bool IsProfiling()
+    {
+        return MikuLuaProfiler.LuaDLL.m_hooked && Application.isPlaying && ptr_luaState != IntPtr.Zero;
+    }
+
+
+    [SLua.MonoPInvokeCallbackAttribute(typeof(SLua.LuaCSFunction))]
+    public void BeginSample(int sampleId)
+    {
+        if (IsProfiling())
+        {
+            LuaProfiler.BeginSample(ptr_luaState, sampleId);
+        }
+    }
+
+    [SLua.MonoPInvokeCallbackAttribute(typeof(SLua.LuaCSFunction))]
+    public void EndSample()
+    {
+        if (IsProfiling())
+        {
+            LuaProfiler.EndSample(LuaProfiler.mainL);
+        }
+    }
+
+    public static string GetSampleName(int sampleId)
+    {
+        return ((ECsLuaProfilerSample)sampleId).ToString();
+    }
+}
+
+public enum ECsLuaProfilerSample
+{
+    None = 0,
+    Sample1,
+    Sample2,
+    Sample3,
+    Sample4,
+}
+
---
+++ LuaProfilerForUnity/Assets/Slua/example/Circle.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using SLua;
+using System;
+
+public class Circle : MonoBehaviour {
+
+
+	LuaSvr svr;
+	LuaTable self;
+	LuaFunction update;
+
+    [CustomLuaClass]
+    public delegate void UpdateDelegate(object self);
+
+    UpdateDelegate ud;
+
+	void Start () {
+		//MikuLuaProfiler.HookLuaSetup.OnStartGame();
+        Debug.Log("Hooked: " + MikuLuaProfiler.LuaDLL.m_hooked);
+        svr = new LuaSvr();
+        svr.init(null, () =>
+        {
+            self = (LuaTable)svr.start("circle/circle");
+            update = (LuaFunction)self["update"];
+            ud = update.cast<UpdateDelegate>();
+        });
+        Debug.Log("Hooked2: " + MikuLuaProfiler.LuaDLL.m_hooked);
+	}
+	
+	void Update () {
+        CsLuaProfiler.CsInstance().BeginSample((int)ECsLuaProfilerSample.Sample3);
+        if (ud != null) ud(self);
+        CsLuaProfiler.CsInstance().EndSample();
+
+    }
+}

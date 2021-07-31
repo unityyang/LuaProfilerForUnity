83f6a2a942dc41f90f8fb54dfe4f5b6f56b71538
unityyang <766684931@qq.com>
2021-08-01 01:33:44 +0800

    整理工程

--- LuaProfilerForUnity/Assets/CsLuaProfiler/ProfilerProxyScript.cs
+++ LuaProfilerForUnity/Assets/CsLuaProfiler/ProfilerProxyScript.cs
@@ -1,10 +1,8 @@
-﻿using MikuLuaProfiler;
-using ProfilerLibrary;
+﻿using ProfilerLibrary;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 public class ProfilerProxyScript : IScriptProxy
 {
